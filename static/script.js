5420803bffc11c7415d19b98660119ee59afa76b
Arnav Krishnan <80376092+arnavkrishnan@users.noreply.github.com>
2025-04-08 13:13:23 -0700

    delete static directory not needed

--- static/script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Page loaded, JS is ready!");
-  
-    // You can add more JS to handle things like mouse-over events, animations, or dynamic content.
-  });
-  
