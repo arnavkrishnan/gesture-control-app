b57f26464b5cdc91fb74dcfce0f8289b529ee5ee
Arnav Krishnan <arnav.s.krishnan@gmail.com>
2025-04-07 23:11:44 -0700

    script and flask

---
+++ static/script.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Page loaded, JS is ready!");
+  
+    // You can add more JS to handle things like mouse-over events, animations, or dynamic content.
+  });
+  
