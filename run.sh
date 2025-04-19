4f72d7a87689b1cfe18ddcc0c11c662033f10af9
Arnav Krishnan <arnav.s.krishnan@gmail.com>
2025-04-19 11:32:02 -0700

    test

--- run.sh
+++ run.sh
@@ -4,16 +4,32 @@ ENV_NAME="gesture-control-app"
 conda info --envs | grep $ENV_NAME
 
 if [ $? -eq 0 ]; then
-    echo "Environment '$ENV_NAME' already exists, activating..."
+    echo "✅ Environment '$ENV_NAME' found. Activating..."
 else
-    echo "Environment '$ENV_NAME' not found, creating a new one..."
+    echo "⚙️ Environment '$ENV_NAME' not found. Creating..."
     conda create --name $ENV_NAME python=3.9 -y
 fi
 
 source ~/miniconda3/bin/activate $ENV_NAME
-pip install opencv-python mediapipe pyautogui numpy
+pip install -r requirements.txt
 
-python app.py
+echo "Which script do you want to run?"
+select option in "Gesture Control" "3D Hand Viewer" "Quit"; do
+    case $option in
+        "Gesture Control")
+            python app.py
+            break
+            ;;
+        "3D Hand Viewer")
+            python hand_3d_visualizer.py
+            break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "Invalid option $REPLY";;
+    esac
+done
 
 conda init
 conda deactivate
